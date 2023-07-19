fec868a7c16f3909b5eee93426621388989adb4d
anomalicoder <hwiunkim@gmail.com>
2023-07-19 23:26:08 +0900

    Update App.js
    Dimentions => getting width, height of the screen
    
    ScrollView => make pages to scroll
    -horizontal : scroll horizontally
    -pagingEnabled : make pages of scroll
    -contentContainerStyle : need to use instead of style in ScrollView
    
    Css things
    -justifyContent : vertical locations
    -alignItems : horizontal locations
    -marginTop(Botton, Left, Right) : make margin on Top(Botton, Left, Right)

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Dimensions  } from 'react-native';
+
+const {height, width:SCREEN_WIDTH} = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={{ flex:1, backgroundColor:"teal" }}>
-      <View style={{flex:1, backgroundColor:"tomato"}}></View>
-      <View style={{flex:7, backgroundColor:"teal"}}></View>
-      <View style={{flex:2, backgroundColor:"orange", borderRadius:50,}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView 
+        horizontal
+        indicatorStyle={{color:'white'}}
+        pagingEnabled
+        contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>15^</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>15^</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>15^</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>15^</Text>
+          <Text style={styles.description}>Rainy</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1, 
+    backgroundColor:"orange",
+  },
+  city:{
+    flex:0.5,
+    backgroundColor:"teal",
+    justifyContent : 'flex-end',
+    alignItems:"center",
+  },
+  cityName:{
+    color:"white",
+    fontSize:50,
+    fontWeight:"300",
+  },
+  weather:{
+  },
+  day:{
+    width:SCREEN_WIDTH,
+    backgroundColor:"teal",
+    alignItems:"center",
+  },
+  temp:{
+    color:'white',
+    marginTop:150,
+    fontSize:180,
+  },
+  description:{
+    color:'white',
+    marginTop:-30,
+    fontSize:50,
+  },
+})
